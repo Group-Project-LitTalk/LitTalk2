8b0ab8d83f8ae593eda1fe021f228a8210ae85b9
lilianofik <lilianofik@users.noreply.github.com>
2021-05-04 14:06:11 -0400

    Lilian's Third commit

---
+++ app/src/main/java/com/example/instagram2/Activities/EditProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.instagram2.Activities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
+}
